20fecd8a0b23e5e80fb57593db20574dff28fedf
Akash <kushwahaakash971@gmail.com>
2023-02-02 12:48:41 +0530

    home-screen-page-UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      Akash
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <header>
+          <div className="header">
+            <Link to="/">Logo</Link>
+          </div>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
