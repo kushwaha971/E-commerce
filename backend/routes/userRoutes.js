253598a871b08eae68c877c80bffbd43f74fdb9a
Akash <kushwahaakash971@gmail.com>
2023-02-07 13:50:07 +0530

    sign In Ui

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,28 +1,27 @@
 import express from "express";
 import User from "../models/userModel.js";
-import bcrypt from 'bcryptjs';
-import expressAsyncHandler from 'express-async-handler';
+import bcrypt from "bcryptjs";
+import expressAsyncHandler from "express-async-handler";
 import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/signin",
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email,password: req.body.password });
-    if (user) {
-    //   if (bcrypt.compare(req.body.password, user.password)) {
-        res.send({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          token: generateToken(user),
-        });
-        return;
-      }
-    // }
-    res.status(401).send({ message: "Invalid email or password" });
-  })
-);
+userRouter.post("/signin", async (req, res) => {
+   const siginUser = await User.findOne({ email: req.body.email, password: req.body.password });
+   if(siginUser){
+    res.send({
+      _id: siginUser.id,
+      name: siginUser.name,
+      email: siginUser.email,
+      isAdmin: siginUser.isAdmin,
+      token: generateToken(siginUser)
+    });
+    console.log(siginUser);
+    return;
+   }
+   else{
+    res.status(401).send({message: 'Invalid Email or Password'})
+   }
+});
+
 export default userRouter;
