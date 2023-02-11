4b896dfaccaf3e9a7287a3d21a39625bb370494b
Akash <kushwahaakash971@gmail.com>
2023-02-11 23:05:42 +0530

    User-Router-Testing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
-import express from "express";
 import User from "../models/userModel.js";
+import express from "express";
 import bcrypt from "bcryptjs";
 import expressAsyncHandler from "express-async-handler";
 import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
+
+// Sign In API
 
 userRouter.post("/signin", async (req, res) => {
   const siginUser = await User.findOne({
@@ -26,22 +28,22 @@ userRouter.post("/signin", async (req, res) => {
   }
 });
 
+// Sign Up API
 userRouter.post(
-  "signup",
+  "/signup",
   expressAsyncHandler(async (req, res) => {
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password),
     });
-
     const user = await newUser.save();
     res.send({
-      _id: user.id,
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(siginUser),
+      token: generateToken(user),
     });
   })
 );
