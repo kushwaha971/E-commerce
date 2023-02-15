76eebecb7871f7114c78a2b00768403aed91394f
Akash <kushwahaakash971@gmail.com>
2023-02-15 15:01:20 +0530

    Creatng-Order-history-API-

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -28,6 +28,18 @@ orderRouter.post(
 );
 
 orderRouter.get(
+  "/mine",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+orderRouter.get(
   "/:id",
   isAuth,
   expressAsyncHandler(async (req, res) => {
